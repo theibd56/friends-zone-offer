25c1e201c831227da45bfa1062ce6edf54a75eb5
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-12-24 20:20:18 +0700

    modal promo has been created

--- main.js
+++ main.js
@@ -7,3 +7,48 @@ import "@fancyapps/ui/dist/fancybox/fancybox.css";
 Fancybox.bind("[data-fancybox]", {
     // Your custom options
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    function showModal() {
+        const modal = document.getElementById('modal-promo');
+        const overlay = document.getElementById('overlay');
+
+        if (modal && overlay) {
+            document.documentElement.classList.add('lock');
+            overlay.style.display = 'block';
+            modal.style.display = 'block';
+            setTimeout(() => {
+                overlay.classList.add('visible');
+                modal.classList.add('visible');
+            }, 10);
+        }
+    }
+
+    function closeModal() {
+        const modal = document.getElementById('modal-promo');
+        const overlay = document.getElementById('overlay');
+
+        if (modal && overlay) {
+            overlay.classList.remove('visible');
+            modal.classList.remove('visible');
+
+            setTimeout(() => {
+                overlay.style.display = 'none';
+                modal.style.display = 'none';
+                document.documentElement.classList.remove('lock');
+            }, 300);
+        }
+    }
+
+    setTimeout(showModal, 5000);
+
+    const closeButton = document.querySelector('.modal-promo__close');
+    if (closeButton) {
+        closeButton.addEventListener('click', closeModal);
+    }
+
+    const overlay = document.getElementById('overlay');
+    if (overlay) {
+        overlay.addEventListener('click', closeModal);
+    }
+});
