44b2181ebbdb70d772c3e503139d1bef7b4e76b4
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-12-24 19:31:43 +0700

    fancybox has been installed

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+import { Fancybox } from "@fancyapps/ui";
+
 import './sass/style.scss'
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+//fancybox (может конфликтовать)
+Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+});
